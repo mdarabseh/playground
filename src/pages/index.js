e79c35229f562f48aa4c037c868ca54fbbef93f3
Amjad.AK <81753930+aabokhashabe@users.noreply.github.com>
2024-07-17 13:32:21 +0000

    Amjad change

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ export default function Home() {
       <div className="relative flex place-items-center ">
         {/* <Counter /> */}
         {/* <Todo /> */}
-        {/* <Supermarket /> */}
-        <TicTacTeo />
+        <Supermarket />
+        {/* <TicTacTeo /> */}
       </div>
 
       {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
